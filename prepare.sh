2eb4c50ecda9eadf825903cdbd0a74d2703960ac
Kangsens™ <malhikambaihaqi@gmail.com>
2026-06-21 21:44:05 +0700

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,4 @@
-mkdir bot-kudeta $$ cd bot-kudeta
-npm init y
-npm install whatsapp-web.js qrcode-terminal
+pkg update && pkg upgrade -y
+pkg install nodejs git python ffmpeg wget -y
+pkg install chromium -y
+npm install -g npm@latest
